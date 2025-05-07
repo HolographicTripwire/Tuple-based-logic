d35d9d960847275cea70ebd1e5cc170b2e912df4
HolographicTripwire <josephsmagen@gmail.com>
2025-05-07 10:25:16 +0100

    Decompose implementation of conjunction introduction

--- proof-verifier/src/deduction_rules/conjunction_introduction.rs
+++ proof-verifier/src/deduction_rules/conjunction_introduction.rs
@@ -9,6 +9,15 @@ pub fn verify_conjunction_introduction(assumptions: &Vec<Proposition>, conclusio
     // Throw an error if there are not three entities in the conclusion
     if conclusion.len() != 3 { return Err(VerificationError::InvalidStepSpecification) }
 
+    verify_conjunction_head()?;
+    verify_conjunction_left()?;
+    verify_conjunction_right()?;
+
+    // If none of the errors were triggered, then this step was successfully verified
+    return Ok(())
+}
+
+fn verify_conjunction_head() -> Result<(),VerificationError> {
     // Panic if the conclusion does not have a first term
     if let PropositionTerm::Entity(entity_id) = conclusion.get_term(0).expect("Conclusion had no first term") {
         // Throw an error if the head is not [BuiltInEntity::Conjunction]
@@ -16,6 +25,10 @@ pub fn verify_conjunction_introduction(assumptions: &Vec<Proposition>, conclusio
     // Throw an error if the head is not a [PropositionTerm::Entity]
     } else { return Err(VerificationError::InvalidStepSpecification) }
 
+    return Ok(());
+}
+
+fn verify_conjunction_left() -> Result<(),VerificationError> {
     // Throw an error if a proposition cannot be constructed by the first term
     if let Ok(conjunction_left) = conclusion.proposition_from_term(1) { 
         // Panic if there is no first assumption
@@ -24,6 +37,10 @@ pub fn verify_conjunction_introduction(assumptions: &Vec<Proposition>, conclusio
             { return Err(VerificationError::InvalidStepSpecification) }
     } else { return Err(VerificationError::InvalidStepSpecification) }
 
+    return Ok(());
+}
+
+fn verify_conjunction_right() -> Result<(),VerificationError> {
     // Throw an error if a proposition cannot be constructed by the second term
     if let Ok(conjunction_right) = conclusion.proposition_from_term(2) { 
         // Panic if there is no second assumption
@@ -32,6 +49,5 @@ pub fn verify_conjunction_introduction(assumptions: &Vec<Proposition>, conclusio
             { return Err(VerificationError::InvalidStepSpecification) }
     } else { return Err(VerificationError::InvalidStepSpecification) }
 
-    // If none of the errors were triggered, then this step was successfully verified
-    return Ok(())
+    Ok(())
 }
