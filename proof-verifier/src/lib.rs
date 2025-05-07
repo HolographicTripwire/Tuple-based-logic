a7f25fbfb86cc24930dae8b4959c244e22f7968f
HolographicTripwire <josephsmagen@gmail.com>
2025-05-07 09:55:11 +0100

    Add rudimentary documentation to proof-verifier/lib.rs

--- proof-verifier/src/lib.rs
+++ proof-verifier/src/lib.rs
@@ -5,19 +5,23 @@ use std::collections::HashSet;
 use deduction_rules::*;
 use shared::{proof::{Proof, ProofStep}, proposition::Proposition};
 
-pub fn verify_proof(assumptions: &HashSet<Proposition>, proof: &Proof) -> Result<(),(usize,VerificationError)> {
+/// Verify that the provided proof is sound under Tuple-Based logic, given some set of starting assumptions
+/// If the proof is not valid, return a [VerificationError] as well as the step that it occurred at
+pub fn verify_proof(proof: &Proof, assumptions: &HashSet<Proposition>) -> Result<(),(usize,VerificationError)> {
     // Create a list of [Proposition] objects which are considered at this time to be true
     let proved = assumptions.clone();
     // Verify each step of the process, throwing an error if any of them fail
     for (i, step) in proof.steps.iter().enumerate() {
-        if let Err(err) = verify_proof_step(&proved, step)
+        if let Err(err) = verify_proof_step(step, &proved)
             { return Err((i,err)) }
     }
     // If all steps were verified, the proof is valid
     Ok(())
 }
 
-fn verify_proof_step(assumptions: &HashSet<Proposition>, step: &ProofStep) -> Result<(),VerificationError> {
+/// Verify that the provided proof step is sound under Tuple-Based Logic, given some set of existing assumptions
+/// If the proof is not valid, return a [VerificationError]
+fn verify_proof_step(step: &ProofStep, assumptions: &HashSet<Proposition>) -> Result<(),VerificationError> {
     // Check whether the step is a valid instance of the type of step it claims to be
     let step_result = verify_proof_step_by_type(&step.step_type, &step.assumptions, &step.conclusion);
     // If it's not a valid instance, throw an Error
