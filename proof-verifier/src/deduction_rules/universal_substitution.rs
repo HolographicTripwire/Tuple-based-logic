7acebec31f497b9456c212725c33020490203bb7
HolographicTripwire <josephsmagen@gmail.com>
2025-05-11 17:33:57 +0100

    Implement universal substitution

--- proof-verifier/src/deduction_rules/universal_substitution.rs
+++ proof-verifier/src/deduction_rules/universal_substitution.rs
@@ -1,7 +1,52 @@
-use shared::{entity::BuiltinEntity, proposition::{Proposition, PropositionTerm}};
+use shared::{atom::BuiltInAtom, proposition::Proposition, term::Term};
 
 use crate::VerificationError;
 
-pub fn verify_universal_instantiation(assumptions: &Vec<Proposition>, conclusion: &Proposition) -> Result<(),VerificationError> {
-    Ok(())
+pub fn verify_universal_substitution(assumptions: &Vec<Proposition>, conclusion: &Proposition) -> Result<(), VerificationError> {
+    // Throw an error if there is not one assumptions
+    let [substitution] = assumptions.as_slice() else { return Err(VerificationError::InvalidStepSpecification) };
+
+    // Throw an error if the assumption is not a tuple
+    let substitution_terms = substitution.0.as_tuple().or(Err(VerificationError::InvalidStepSpecification))?;
+    // Throw an error if there are not three terms in the conclusion
+    let [substitution_head, term_to_replace, term_to_replace_within] = substitution_terms.as_slice() else { return Err(VerificationError::InvalidStepSpecification) };
+
+    // Throw an error if the head of the substitution is incorrect
+    if substitution_head != &BuiltInAtom::UniversalQuantifier.into() { return Err(VerificationError::InvalidStepSpecification) }
+    // Check that remainder of the substitution is correct
+    substitution_comparison(term_to_replace_within, term_to_replace, &conclusion.0)?;
+    
+    // If none of the errors were triggered, then this step was successfully verified
+    return Ok(())
+}
+
+fn substitution_comparison(find_term: &Term, replace_term: &Term, verify_term: &Term) -> Result<Option<Term>,VerificationError> {
+    // If the find term is the replace term, then it must have been replaced with the verify term so return that
+    if find_term == replace_term { return Ok(Some(verify_term.clone())) }
+    
+    // Throw an error if find_term or verify_term is not a tuple
+    let find_terms = find_term.as_tuple().or(Err(VerificationError::InvalidStepSpecification))?;
+    let verify_terms = verify_term.as_tuple().or(Err(VerificationError::InvalidStepSpecification))?;
+    // Throw an error if the find term and verify terms are of different lengths (a substitution would not resolve this)
+    if find_terms.len() != verify_terms.len() { return Err(VerificationError::InvalidStepSpecification) }
+    
+    // Recurse, performing substitution comparison on each term within the sets of tuples
+    let results = find_terms.iter().zip(verify_terms.iter())
+        .map(|(term1, term2)| -> Result<Option<Term>,VerificationError> { substitution_comparison(term1, replace_term, term2) });
+    
+    // Filter out unwanted results
+    let mut filtered_results = Vec::new();
+    for result in results { match result {
+        Ok(Some(term)) => filtered_results.push(term),
+        Ok(None) => {}, // Ignore all None values - as these indicate that there were no replacements made, nor errors encountered
+        Err(err) => { return Err(err) }, // Throw an error if any of the substitution comparisons returned an error
+    }}
+
+    if let Some(first_result) = filtered_results.get(0).cloned() {
+        // Throw an error if the same replacement was not made on each term within the set of tuples
+        for result in filtered_results { if result != first_result { return Err(VerificationError::InvalidStepSpecification) } }
+        // If all of the replacements are the same, then return what they were all replaced with
+        return Ok(Some(first_result))
+    // If no replacements were made, then return None; the find and verify term are the same
+    } else { return Ok(None) }
 }
