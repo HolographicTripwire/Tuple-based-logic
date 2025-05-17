47ee5dd2532d646a36bc10f9c2e67b49065a4e7e
HolographicTripwire <josephsmagen@gmail.com>
2025-05-17 15:20:17 +0100

    Remove redundant verification_error file (since replaced with validation_error

--- proof-verifier/src/verification_error.rs
+++
@@ -1,8 +0,0 @@
-use shared::proposition::PropositionSet;
-
-#[derive(Clone)]
-pub enum ProofVerificationError {
-    AssumptionsNotFound(PropositionSet),
-    ConclusionsNotFound(PropositionSet),
-    InvalidStepSpecification,
-}
