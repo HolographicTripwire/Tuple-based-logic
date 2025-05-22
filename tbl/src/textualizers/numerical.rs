620a905100d5c85094e5bcb71ada4eec45447a5e
HolographicTripwire <josephsmagen@gmail.com>
2025-05-22 12:04:02 +0100

    Implement tests for numerical term textualizer

--- tbl/src/textualizers/numerical.rs
+++ tbl/src/textualizers/numerical.rs
@@ -22,3 +22,39 @@ pub static TERM_TEXTUALIZER: LazyLock<TermTextualizer> = LazyLock::new(|| -> Ter
     Box::new(VecTextualizer()),
     Box::new(NoRulesTextualizer())
 )});
+
+#[cfg(test)]
+mod tests {
+    use shared::propositions::{Term};
+
+    use super::*;
+
+    #[test]
+    fn test_textualize_atom() {
+        let term = Term::from(AtomId::try_from(0).unwrap());
+        assert_eq!(TERM_TEXTUALIZER.to_text(&term),Ok("0".to_string()));
+    }
+
+    #[test]
+    fn test_textualize_unary_tuple() {
+        let atom_0 = Term::from(AtomId::try_from(0).unwrap());
+        let term = Term::from(vec![atom_0]);
+        assert_eq!(TERM_TEXTUALIZER.to_text(&term),Ok("(0)".to_string()));
+    }
+
+    #[test]
+    fn test_textualize_binary_tuple() {
+        let atom_0 = Term::from(AtomId::try_from(0).unwrap());
+        let atom_1 = Term::from(AtomId::try_from(1).unwrap());
+        let term = Term::from(vec![atom_0,atom_1]);
+        assert_eq!(TERM_TEXTUALIZER.to_text(&term),Ok("(0, 1)".to_string()));
+    }
+
+    #[test]
+    fn test_textualize_nested_tuple() {
+        let atom_0 = Term::from(AtomId::try_from(0).unwrap());
+        let atom_1 = Term::from(AtomId::try_from(1).unwrap());
+        let term = Term::from(vec![Term::from(vec![atom_0]),atom_1]);
+        assert_eq!(TERM_TEXTUALIZER.to_text(&term),Ok("((0), 1)".to_string()));
+    }
+}
