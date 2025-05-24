b4e86d6eac52e36ea770713a179d2ead7f443500
HolographicTripwire <josephsmagen@gmail.com>
2025-05-24 12:06:34 +0100

    Remove test code that was accidentally left in stringification/src/lib.rs

--- stringification/src/lib.rs
+++ stringification/src/lib.rs
@@ -12,33 +12,3 @@ pub trait Stringify<Object>: Sync + Send {
 pub trait Destringify<Object>: Send + Sync {
     fn destringify(&self, string: &String) -> Result<Object,()>;
 }
-
-
-trait MyTrait {
-    fn do_something(&self);
-    fn clone_box(&self) -> Box<dyn MyTrait>; // Method to clone the trait object
-}
-
-#[derive(Clone)]
-struct MyStruct;
-
-impl MyTrait for MyStruct {
-    fn do_something(&self) {
-        println!("Doing something!");
-    }
-
-    fn clone_box(&self) -> Box<dyn MyTrait> {
-        Box::new(self.clone()) // Clone the concrete type
-    }
-}
-
-struct Container {
-    my_obj: Box<dyn MyTrait>,
-}
-
-impl Container {
-    // Getter that returns a clone of my_obj
-    fn get_my_obj(&self) -> Box<dyn MyTrait> {
-        self.my_obj.clone_box() // Use the clone_box method
-    }
-}
