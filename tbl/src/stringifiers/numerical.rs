ad08483f01ff4c64f8341a55deba764b9639efcc
HolographicTripwire <josephsmagen@gmail.com>
2025-05-24 12:36:45 +0100

    Improve testing for numerical stringification

--- tbl/src/stringifiers/numerical.rs
+++ tbl/src/stringifiers/numerical.rs
@@ -32,36 +32,95 @@ pub static TERM_STRINGIFIER: LazyLock<Box<dyn Stringifier<Term>>> =
 
 #[cfg(test)]
 mod tests {
+    use std::{cell::LazyCell, collections::HashMap};
+
     use tbl_structures::propositions::{Term};
 
     use super::*;
 
+    static CONVERSIONS: LazyLock<HashMap<&str,Term>> = LazyLock::new(|| -> HashMap<&str,Term> { 
+        let atom = |u: usize| { Term::from(AtomId::try_from(u).unwrap()) };
+        HashMap::from_iter(vec![
+            ("0",atom(0)),
+            ("(0)",Term::Tuple(vec![atom(0)])),
+            ("(0, 1)",Term::Tuple(vec![atom(0),atom(1)])),
+            ("((0), 1)", Term::from(vec![Term::from(vec![atom(0)]),atom(1)])),
+        ].into_iter())
+    });
+
     #[test]
-    fn test_textualize_atom() {
-        let term = Term::from(AtomId::try_from(0).unwrap());
-        assert_eq!(TERM_STRINGIFIER.stringify(&term),Ok("0".to_string()));
+    fn test_stringify_atom() {
+        // Get the string and term to test
+        let str = "0";
+        let term = CONVERSIONS.get(str).unwrap();
+        // Test stringification
+        let term_stringified = TERM_STRINGIFIER.stringify(&term);
+        assert_eq!(term_stringified,Ok(str.to_string()));
+    }
+    #[test]
+    fn test_destringify_atom() {
+        // Get the string and term to test
+        let str = "0";
+        let term = CONVERSIONS.get(str).unwrap();
+        // Test destringification
+        let str_destringified = TERM_STRINGIFIER.destringify(&str.to_string());
+        assert_eq!(Ok(term.clone()),str_destringified);
     }
 
     #[test]
-    fn test_textualize_unary_tuple() {
-        let atom_0 = Term::from(AtomId::try_from(0).unwrap());
-        let term = Term::from(vec![atom_0]);
-        assert_eq!(TERM_STRINGIFIER.stringify(&term),Ok("(0)".to_string()));
+    fn test_stringify_unary_tuple() {
+        // Get the string and term to test
+        let str = "(0)";
+        let term = CONVERSIONS.get(str).unwrap();
+        // Test stringification
+        let term_stringified = TERM_STRINGIFIER.stringify(&term);
+        assert_eq!(term_stringified,Ok(str.to_string()));
+    }
+    #[test]
+    fn test_destringify_unary_tuple() {
+        // Get the string and term to test
+        let str = "(0)";
+        let term = CONVERSIONS.get(str).unwrap();
+        // Test destringification
+        let str_destringified = TERM_STRINGIFIER.destringify(&str.to_string());
+        assert_eq!(Ok(term.clone()),str_destringified);
     }
 
     #[test]
-    fn test_textualize_binary_tuple() {
-        let atom_0 = Term::from(AtomId::try_from(0).unwrap());
-        let atom_1 = Term::from(AtomId::try_from(1).unwrap());
-        let term = Term::from(vec![atom_0,atom_1]);
-        assert_eq!(TERM_STRINGIFIER.stringify(&term),Ok("(0, 1)".to_string()));
+    fn test_stringify_binary_tuple() {
+        // Get the string and term to test
+        let str = "(0, 1)";
+        let term = CONVERSIONS.get(str).unwrap();
+        // Test stringification
+        let term_stringified = TERM_STRINGIFIER.stringify(&term);
+        assert_eq!(term_stringified,Ok(str.to_string()));
+    }
+    #[test]
+    fn test_destringify_binary_tuple() {
+        // Get the string and term to test
+        let str = "(0, 1)";
+        let term = CONVERSIONS.get(str).unwrap();
+        // Test destringification
+        let str_destringified = TERM_STRINGIFIER.destringify(&str.to_string());
+        assert_eq!(Ok(term.clone()),str_destringified);
     }
 
     #[test]
-    fn test_textualize_nested_tuple() {
-        let atom_0 = Term::from(AtomId::try_from(0).unwrap());
-        let atom_1 = Term::from(AtomId::try_from(1).unwrap());
-        let term = Term::from(vec![Term::from(vec![atom_0]),atom_1]);
-        assert_eq!(TERM_STRINGIFIER.stringify(&term),Ok("((0), 1)".to_string()));
+    fn test_stringify_nested_tuple() {
+        // Get the string and term to test
+        let str = "((0), 1)";
+        let term = CONVERSIONS.get(str).unwrap();
+        // Test stringification
+        let term_stringified = TERM_STRINGIFIER.stringify(&term);
+        assert_eq!(term_stringified,Ok(str.to_string()));
+    }
+    #[test]
+    fn test_destringify_nested_tuple() {
+        // Get the string and term to test
+        let str = "((0), 1)";
+        let term = CONVERSIONS.get(str).unwrap();
+        // Test destringification
+        let str_destringified = TERM_STRINGIFIER.destringify(&str.to_string());
+        assert_eq!(Ok(term.clone()),str_destringified);
     }
 }
