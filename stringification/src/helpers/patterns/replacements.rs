ac7066f327d97011648ac3ab95f6de7e238d66ad
HolographicTripwire <josephsmagen@gmail.com>
2025-06-01 19:40:02 +0100

    Rename ExprPatternReplacements' methods with more fitting names.

--- stringification/src/helpers/patterns/replacements.rs
+++ stringification/src/helpers/patterns/replacements.rs
@@ -12,7 +12,7 @@ impl ExprPatternReplacements {
         vars_to_vals: HashMap::new()
     }}
 
-    pub fn add_var_from_val(&mut self, k: String, v: String) -> Result<(),()> {
+    pub fn add_var_to_val(&mut self, k: String, v: String) -> Result<(),()> {
         if self.vars.contains(&k) { return Err(()); }
         self.vars.insert(k.clone());
         self.var_to_val.insert(k, v);
@@ -21,7 +21,7 @@ impl ExprPatternReplacements {
     pub fn get_va1_from_var(&self, s: &String) -> Option<&String>
         { self.var_to_val.get(s) }
 
-    pub fn add_vars_from_val(&mut self, k1: String, k2: String, v: Vec<String>) -> Result<(),()> { 
+    pub fn add_vars_to_vals(&mut self, k1: String, k2: String, v: Vec<String>) -> Result<(),()> { 
         if self.vars.contains(&k1) || self.vars.contains(&k2) { return Err(()); }
         self.vars_to_vals.insert((k1,k2), v);
         return Ok(());
