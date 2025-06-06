7b74e0470d44441752641e2e36f3a9d26f238958
HolographicTripwire <josephsmagen@gmail.com>
2025-06-06 12:49:04 +0100

    Write tests for TupleOrError

--- structures/src/propositions/tuple_or_error.rs
+++ structures/src/propositions/tuple_or_error.rs
@@ -27,3 +27,73 @@ impl <E: Clone> TupleOrError<E> {
         expr.as_slice().or(Err(self.error.clone()))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::atoms::AtomId;
+
+    use super::*;
+
+    fn atomic_expression_vec(nums: Vec<usize>) -> Vec<Expression> {
+        nums.iter()
+            .map(|num| -> Expression {
+                Expression::Atomic(AtomId::try_from(*num).unwrap())
+            }).collect()
+    }
+
+    #[test]
+    fn test_prop_as_tuple_with_atom() {
+        let expression = Expression::Atomic((AtomId::try_from(1)).unwrap());
+        let proposition = Proposition(expression);
+        assert_eq!(TUPLE_OR_NONE.prop_as_tuple(&proposition), Err(()));
+    }
+
+    #[test]
+    fn test_prop_as_tuple_with_tuple() {
+        let expressions = atomic_expression_vec(vec![0,1,2]);
+        let combined_expression = Expression::Tuple(expressions.clone());
+        let proposition = Proposition(combined_expression);
+        assert_eq!(TUPLE_OR_NONE.prop_as_tuple(&proposition), Ok(&expressions));
+    }
+
+    #[test]
+    fn test_expr_as_tuple_with_atom() {
+        let expression = Expression::Atomic((AtomId::try_from(1)).unwrap());
+        assert_eq!(TUPLE_OR_NONE.expr_as_tuple(&expression), Err(()));
+    }
+
+    #[test]
+    fn test_expr_as_tuple_with_tuple() {
+        let expressions = atomic_expression_vec(vec![0,1,2]);
+        let combined_expression = Expression::Tuple(expressions.clone());
+        assert_eq!(TUPLE_OR_NONE.expr_as_tuple(&combined_expression), Ok(&expressions));
+    }
+
+    #[test]
+    fn test_prop_as_slice_with_atom() {
+        let expression = Expression::Atomic((AtomId::try_from(1)).unwrap());
+        let proposition = Proposition(expression);
+        assert_eq!(TUPLE_OR_NONE.prop_as_slice(&proposition), Err(()));
+    }
+
+    #[test]
+    fn test_prop_as_slice_with_tuple() {
+        let expressions = atomic_expression_vec(vec![0,1,2]);
+        let combined_expression = Expression::Tuple(expressions.clone());
+        let proposition = Proposition(combined_expression);
+        assert_eq!(TUPLE_OR_NONE.prop_as_slice(&proposition), Ok(expressions.as_slice()));
+    }
+
+    #[test]
+    fn test_expr_as_slice_with_atom() {
+        let expression = Expression::Atomic((AtomId::try_from(1)).unwrap());
+        assert_eq!(TUPLE_OR_NONE.expr_as_slice(&expression), Err(()));
+    }
+
+    #[test]
+    fn test_expr_as_slice_with_tuple() {
+        let expressions = atomic_expression_vec(vec![0,1,2]);
+        let combined_expression = Expression::Tuple(expressions.clone());
+        assert_eq!(TUPLE_OR_NONE.expr_as_slice(&combined_expression), Ok(expressions.as_slice()));
+    }
+}
