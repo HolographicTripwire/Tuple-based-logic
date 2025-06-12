266dc4484325352ad8ccedad6f33d17fb24ebcde
HolographicTripwire <josephsmagen@gmail.com>
2025-06-12 09:37:40 +0100

    Update parse_all_str to use HashSet rather than Vec in accordance with parsertools update

--- textualization/src/lib.rs
+++ textualization/src/lib.rs
@@ -5,15 +5,15 @@ pub mod generation;
 
 #[cfg(test)]
 mod test_helpers {
-    use std::{fmt::Debug, hash::Hash};
+    use std::collections::HashSet;
 
-    use parsertools::{Parser, ParseError};
+    use parsertools::{AstBounds, ParseError, Parser};
 
-    pub (crate) fn parse_str<T: PartialEq + Debug + Hash + Clone>(parser: Parser<char,T>, string: &str) -> Result<T,ParseError<char>> {
+    pub (crate) fn parse_str<T: AstBounds>(parser: Parser<char,T>, string: &str) -> Result<T,ParseError<char>> {
         let chars = string.chars().collect::<Vec<_>>();
         parser.parse(&chars)
     }
-    pub (crate) fn parse_all_str<T: PartialEq + Debug + Hash + Clone>(parser: Parser<char,T>, string: &str) -> Result<T,ParseError<char>> {
+    pub (crate) fn parse_all_str<T: AstBounds>(parser: Parser<char,T>, string: &str) -> HashSet<T> {
         let chars = string.chars().collect::<Vec<_>>();
         parser.parse_all(&chars)
     }
