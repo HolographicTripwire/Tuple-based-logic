ae6f535c26aedc1e4c8bb5b3405133b4d8133c51
HolographicTripwire <josephsmagen@gmail.com>
2025-06-12 09:51:55 +0100

    Update ExprPatternComponent parsers to use referenfces where possible

--- textualization/src/structures/expressions/patterns/components.rs
+++ textualization/src/structures/expressions/patterns/components.rs
@@ -1,4 +1,4 @@
-use parsertools::{lazy, pred, Parser};
+use parsertools::{lazy, Parser};
 
 use crate::helpers::{string_parser, word_parser};
 
@@ -15,7 +15,7 @@ impl ExprPatternComponent {
 }
 
 #[derive(PartialEq,Eq,Clone,Hash,Debug)]
-enum ExprPatternAssignment {
+pub enum ExprPatternAssignment {
     Constant,
     Variable(String,String),
     Variables((String,String),Vec<String>)
@@ -29,21 +29,21 @@ impl ExprPatternAssignment {
         { Self::Variables((var_1.to_string(), var_2.to_string()), vals.iter().map(|s| s.to_string()).collect()) }
 }
 
-fn pattern_component_parser<'a>(component: ExprPatternComponent) -> Parser<'a,char,ExprPatternAssignment> {
+pub fn pattern_component_parser<'a>(component: &'a ExprPatternComponent) -> Parser<'a,char,ExprPatternAssignment> {
     match component {
         ExprPatternComponent::Constant(str) => const_parser(str),
         ExprPatternComponent::Variable(var) => var_parser(var),
         ExprPatternComponent::Variables(vars, joiner) => vars_parser(vars, joiner),
     }
 }
-fn const_parser<'a>(str: String) -> Parser<'a,char,ExprPatternAssignment> {
+fn const_parser<'a>(str: &String) -> Parser<'a,char,ExprPatternAssignment> {
     string_parser(&str).unwrap().map(|_| ExprPatternAssignment::Constant)
 }
-fn var_parser<'a>(var: String) -> Parser<'a,char,ExprPatternAssignment> {
+fn var_parser<'a>(var: &'a String) -> Parser<'a,char,ExprPatternAssignment> {
     word_parser().map(move |val| ExprPatternAssignment::Variable(var.clone(), val))
 }
-fn vars_parser<'a>(vars: (String, String), joiner: String) -> Parser<'a,char,ExprPatternAssignment> {
-    vars_parser_inner(joiner).map(move |vals| ExprPatternAssignment::Variables(vars.clone(), vals))
+fn vars_parser<'a>(vars: &'a (String, String), joiner: &'a String) -> Parser<'a,char,ExprPatternAssignment> {
+    vars_parser_inner(joiner.clone()).map(move |vals| ExprPatternAssignment::Variables(vars.clone(), vals))
 }
 fn vars_parser_inner<'a>(joiner: String) -> Parser<'a,char,Vec<String>> {
     let single_var = word_parser().map(|val| vec![val]);
@@ -56,8 +56,6 @@ fn vars_parser_inner<'a>(joiner: String) -> Parser<'a,char,Vec<String>> {
 mod tests {
     use std::collections::HashSet;
 
-    use parsertools::ParseError;
-
     use crate::{structures::expressions::patterns::components::{pattern_component_parser, ExprPatternAssignment, ExprPatternComponent}, test_helpers::{parse_all_str, parse_str}};
 
     #[test]
@@ -69,7 +67,7 @@ mod tests {
     fn test_parser_with_constant_match() {
         assert_eq!(
             parse_str(
-                pattern_component_parser(ExprPatternComponent::new_const("Hello")), 
+                pattern_component_parser(&ExprPatternComponent::new_const("Hello")), 
                 "Hello"
             ), Ok(ExprPatternAssignment::new_const())
         )
@@ -79,7 +77,7 @@ mod tests {
     fn test_parser_with_constant_nonmatch() {
         assert!(
             parse_str(
-                pattern_component_parser(ExprPatternComponent::new_const("Hello")), 
+                pattern_component_parser(&ExprPatternComponent::new_const("Hello")), 
                 "Hello there"
             ).is_err()
         )
@@ -89,7 +87,7 @@ mod tests {
     fn test_parser_with_var() {
         assert_eq!(
             parse_str(pattern_component_parser(
-                ExprPatternComponent::new_var("Marco")),
+                &ExprPatternComponent::new_var("Marco")),
                 "Polo"
             ), Ok(ExprPatternAssignment::new_var("Marco","Polo"))
         )
@@ -99,7 +97,7 @@ mod tests {
     fn test_parser_with_vars_1() {
         assert_eq!(
             parse_str(pattern_component_parser(
-                ExprPatternComponent::new_vars("A"," and ","B")),
+                &ExprPatternComponent::new_vars("A"," and ","B")),
                 "Sugar"
             ), Ok(ExprPatternAssignment::new_vars("A","B",vec!["Sugar"]))
         )
@@ -109,7 +107,7 @@ mod tests {
     fn test_parser_with_vars_2() {
         assert_eq!(
             parse_all_str(
-                pattern_component_parser(ExprPatternComponent::new_vars("A"," and ","B")),
+                pattern_component_parser(&ExprPatternComponent::new_vars("A"," and ","B")),
                 "Sugar and Spice"
             ), HashSet::from([
                 ExprPatternAssignment::new_vars("A","B",vec!["Sugar","Spice"]),
@@ -122,7 +120,7 @@ mod tests {
     fn test_parser_with_vars_3() {
         assert_eq!(
             parse_all_str(
-                pattern_component_parser(ExprPatternComponent::new_vars("A"," and ","B")),
+                pattern_component_parser(&ExprPatternComponent::new_vars("A"," and ","B")),
                 "Sugar and Spice and Everything nice"
             ), HashSet::from([
                 ExprPatternAssignment::new_vars("A","B",vec!["Sugar","Spice", "Everything nice"]),
