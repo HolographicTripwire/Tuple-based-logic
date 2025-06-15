05730b247194c4e65e5e64010cb0ac879bee0316
HolographicTripwire <josephsmagen@gmail.com>
2025-06-15 16:00:38 +0100

    Implement expression_parser

--- textualization/src/structures/expressions/mod.rs
+++ textualization/src/structures/expressions/mod.rs
@@ -1,3 +1,36 @@
-pub mod raw;
-pub mod special_case;
+use parsertools::parsers::{helpers::lazy, tokens::pred, Parser};
+use tbl_structures::propositions::Expression;
 
+use crate::structures::expressions::raw::{raw_expression_parser, RawExpressionControls};
+
+pub mod raw;
+pub mod patterns;
+
+pub struct ExpressionControls<'a> {
+    raw_controls: RawExpressionControls,
+    special_cases: Vec<SpecialCase<'a>>,
+}
+impl <'a> ExpressionControls<'a> {
+    pub fn raw_controls(&self) -> &RawExpressionControls { &self.raw_controls }
+    pub fn special_cases(&self) -> &Vec<SpecialCase<'a>> { &self.special_cases }
+}
+
+pub struct SpecialCase<'a>(Box<dyn 'a + Sync + Send + Fn(Parser<'a,char,Expression>) -> Parser<'a,char,Expression>>);
+impl <'a>SpecialCase<'a> {
+    pub fn new<F: 'a + Fn(Parser<'a,char,Expression>) -> Parser<'a,char,Expression> + Sync + Send>(case: F) -> Self
+        { Self(Box::new(case)) }
+    pub fn construct(&self, expression_parser: Parser<'a,char,Expression>) -> Parser<'a,char,Expression>
+        { (self.0)(expression_parser) }
+}
+
+pub fn expression_parser<'a>(controls: &'a ExpressionControls<'a>) -> Parser<'a,char,Expression> {
+    raw_expression_parser(controls.raw_controls())
+        .or(processed_expression_parser(controls))
+}
+
+fn processed_expression_parser<'a>(controls: &'a ExpressionControls<'a>) -> Parser<'a,char,Expression> {
+    let expression_parser = lazy(|| expression_parser(controls));
+    let iter = controls.special_cases().iter().map(move |case| case.construct(expression_parser.clone()));
+    iter.reduce(|acc, next| acc.or(next))
+        .unwrap_or(pred(|_| None))
+}
