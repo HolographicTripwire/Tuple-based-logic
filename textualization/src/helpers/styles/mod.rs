c189a1be0736e1897a6879016cca8970ed7f6cc3
HolographicTripwire <josephsmagen@gmail.com>
2025-06-18 10:12:58 +0100

    Create Style and Stylable traits, as well as Styled struct

---
+++ textualization/src/helpers/styles/mod.rs
@@ -0,0 +1,25 @@
+use std::{collections::HashSet, fmt::Display};
+
+pub trait Style<Sb: Stylable>: Sized + Clone {
+    fn controls(&self) -> HashSet<String>;
+    fn stringify(&self, stylable: Sb) -> String;
+}
+
+pub trait Stylable: Sized + Clone {
+    fn styled<S: Style<Self>>(&self, style: S) -> Styled<Self,S>
+        { Styled::new(self.clone(), style.clone()) }
+}
+impl <Sb: Sized + Clone> Stylable for Sb {}
+
+pub struct Styled<Sb: Stylable, S: Style<Sb>> {
+    stylable: Sb,
+    style: S
+}
+impl <Sb: Stylable, S: Style<Sb>> Styled<Sb,S> {
+    fn new(stylable: Sb, style: S) -> Self { Self { stylable, style } }
+}
+impl <Sb: Stylable, S: Style<Sb>> Display for Styled<Sb,S> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({})", self.style.stringify(self.stylable))
+    }
+}
