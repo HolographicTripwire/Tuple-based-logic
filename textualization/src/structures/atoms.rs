aa6a73b718f66ae5c0260b4ca1f3855f124856f9
HolographicTripwire <josephsmagen@gmail.com>
2025-06-20 16:53:53 +0100

    Fully implement Style for AtomStyle

--- textualization/src/structures/atoms.rs
+++ textualization/src/structures/atoms.rs
@@ -23,15 +23,15 @@ impl AtomStyle {
     pub fn to_id(&self, num: usize) -> String { self.atom_id_indicator.clone() + &num.to_string() }
 }
 impl Style<AtomId> for AtomStyle {
-    fn stringify(&self, stylable: &AtomId) -> String {
-        todo!()
+    fn stringify(&self, atom: &AtomId) -> String {
+        self.to_id(atom.0 as usize)
     }
-}
-
-pub fn atom_id_parser<'a>(style: &AtomStyle) -> Parser<'a, char,AtomId> {
-    string_parser(style.id_indicator()).unwrap().then(
-        num_parser()
-    ).map(|(_, uint)| -> AtomId { AtomId::try_from(uint).unwrap() })
+    
+    fn parser<'a>(&self) -> Parser<'a,char,AtomId> {
+        string_parser(self.id_indicator()).unwrap().then(
+            num_parser()
+        ).map(|(_, uint)| -> AtomId { AtomId::try_from(uint).unwrap() })
+    }
 }
 
 #[cfg(test)]
@@ -48,14 +48,14 @@ pub (crate) mod tests {
     
     #[test]
     fn test_id_parser_with_atom_id() {
-        assert_eq!(parse_str(atom_id_parser(&TEST_ATOM_STYLE), "#1124"),Ok(AtomId(1124)))
+        assert_eq!(parse_str(TEST_ATOM_STYLE.parser(), "#1124"),Ok(AtomId(1124)))
     }
     #[test]
     fn test_id_parser_with_plain_num() {
-        assert!(parse_str(atom_id_parser(&TEST_ATOM_STYLE), "1124").is_err())
+        assert!(parse_str(TEST_ATOM_STYLE.parser(), "1124").is_err())
     }
     #[test]
     fn test_id_parser_with_atom_symbol() {
-        assert!(parse_str(atom_id_parser(&TEST_ATOM_STYLE), "P").is_err())
+        assert!(parse_str(TEST_ATOM_STYLE.parser(), "P").is_err())
     }
 }
