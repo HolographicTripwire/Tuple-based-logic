61db2c53b2fe2774661ecfa4b1aa5b7b6033f7de
HolographicTripwire <josephsmagen@gmail.com>
2025-06-20 12:34:58 +0100

    Implement non-variadic function generation for SpecialCaseBuilder to allow arity specification

--- textualization/src/structures/expressions/functional/builder.rs
+++ textualization/src/structures/expressions/functional/builder.rs
@@ -19,33 +19,63 @@ impl SpecialCasesBuilder {
         self
     }
 
-    pub fn add_atomic_prefix_function(mut self, atom_id: usize, nonfix_symbol: &str, prefix_symbol: &str) -> Self {
+    pub fn add_variadic_atomic_prefix_function(mut self, atom_id: usize, nonfix_symbol: &str, prefix_symbol: &str) -> Self {
         self.atomic_symbols.push(symbol_atom(atom_id, nonfix_symbol, &self.style));
-        self.atomic_symbols.push(prefix_function(nonfix_symbol, prefix_symbol, &self.style));
+        self.atomic_symbols.push(variadic_prefix_function(nonfix_symbol, prefix_symbol, &self.style));
         self
     }
-
-    pub fn add_atomic_infix_function(mut self, atom_id: usize, nonfix_symbol: &str, infix_symbol: &str) -> Self {
+    pub fn add_atomic_prefix_function<I: IntoIterator<Item = usize>>(mut self, atom_id: usize, arities: I, nonfix_symbol: &str, prefix_symbol: &str) -> Self {
         self.atomic_symbols.push(symbol_atom(atom_id, nonfix_symbol, &self.style));
-        self.atomic_symbols.push(infix_function(nonfix_symbol, &infix_symbol, &self.style));
-        self
+        for arity in arities.into_iter() {
+            self.atomic_symbols.push(prefix_function(nonfix_symbol, arity, prefix_symbol, &self.style));
+        } self
     }
 
-    pub fn add_atomic_postfix_function(mut self, atom_id: usize, nonfix_symbol: &str, postfix_symbol: &str) -> Self {
+    pub fn add_variadic_atomic_infix_function(mut self, atom_id: usize, nonfix_symbol: &str, infix_symbol: &str) -> Self {
         self.atomic_symbols.push(symbol_atom(atom_id, nonfix_symbol, &self.style));
-        self.atomic_symbols.push(postfix_function(nonfix_symbol, postfix_symbol, &self.style));
+        self.atomic_symbols.push(variadic_infix_function(nonfix_symbol, &infix_symbol, &self.style));
         self
     }
-
-    pub fn add_atomic_outfix_function(mut self, atom_id: usize, nonfix_symbol: &str, left_symbol: &str, right_symbol: &str) -> Self {
+    pub fn add_atomic_infix_function<I: IntoIterator<Item = usize>>(mut self, atom_id: usize, arities: I, nonfix_symbol: &str, infix_symbol: &str) -> Self {
         self.atomic_symbols.push(symbol_atom(atom_id, nonfix_symbol, &self.style));
-        self.atomic_symbols.push(outfix_function(nonfix_symbol, left_symbol, right_symbol, &self.style));
-        self
+        for arity in arities.into_iter() {
+            self.atomic_symbols.push(infix_function(nonfix_symbol, arity, &infix_symbol, &self.style));
+        } self
     }
 
-    pub fn add_atomic_allfix_function(mut self, atom_id: usize, nonfix_symbol: &str, left_symbol: &str, infix_symbol: &str, right_symbol: &str) -> Self {
+    pub fn add_variadic_atomic_postfix_function(mut self, atom_id: usize, nonfix_symbol: &str, postfix_symbol: &str) -> Self {
         self.atomic_symbols.push(symbol_atom(atom_id, nonfix_symbol, &self.style));
-        self.atomic_symbols.push(allfix_function(nonfix_symbol, left_symbol, infix_symbol, right_symbol, &self.style));
+        self.atomic_symbols.push(variadic_postfix_function(nonfix_symbol, postfix_symbol, &self.style));
         self
+    }
+    pub fn add_atomic_postfix_function<I: IntoIterator<Item = usize>>(mut self, atom_id: usize, arities: I, nonfix_symbol: &str, postfix_symbol: &str) -> Self {
+        self.atomic_symbols.push(symbol_atom(atom_id, nonfix_symbol, &self.style));
+        for arity in arities.into_iter() {
+            self.atomic_symbols.push(postfix_function(nonfix_symbol, arity, postfix_symbol, &self.style));
+        } self
+    }
+
+    pub fn add_variadic_atomic_outfix_function(mut self, atom_id: usize, nonfix_symbol: &str, left_symbol: &str, right_symbol: &str) -> Self {
+        self.atomic_symbols.push(symbol_atom(atom_id, nonfix_symbol, &self.style));
+        self.atomic_symbols.push(variadic_outfix_function(nonfix_symbol, left_symbol, right_symbol, &self.style));
+        self
+    }
+    pub fn add_atomic_outfix_function<I: IntoIterator<Item = usize>>(mut self, atom_id: usize, arities: I, nonfix_symbol: &str, left_symbol: &str, right_symbol: &str) -> Self {
+        self.atomic_symbols.push(symbol_atom(atom_id, nonfix_symbol, &self.style));
+        for arity in arities.into_iter() {
+            self.atomic_symbols.push(outfix_n_function(nonfix_symbol, arity, left_symbol, right_symbol, &self.style));
+        } self
+    }
+    
+    pub fn add_variadic_atomic_allfix_function(mut self, atom_id: usize, nonfix_symbol: &str, left_symbol: &str, infix_symbol: &str, right_symbol: &str) -> Self {
+        self.atomic_symbols.push(symbol_atom(atom_id, nonfix_symbol, &self.style));
+        self.atomic_symbols.push(variadic_allfix_function(nonfix_symbol, left_symbol, infix_symbol, right_symbol, &self.style));
+        self
+    }
+    pub fn add_atomic_allfix_function<I: IntoIterator<Item = usize>>(mut self, atom_id: usize, arities: I, nonfix_symbol: &str, left_symbol: &str, infix_symbol: &str, right_symbol: &str) -> Self {
+        self.atomic_symbols.push(symbol_atom(atom_id, nonfix_symbol, &self.style));
+        for arity in arities.into_iter() {
+            self.atomic_symbols.push(allfix_function(nonfix_symbol, arity, left_symbol, infix_symbol, right_symbol, &self.style));
+        } self
     }
 }
--- textualization/src/structures/expressions/functional/special_case.rs
+++ textualization/src/structures/expressions/functional/special_case.rs
@@ -12,39 +12,72 @@ pub(super) fn symbol(before: &str, after: &str) -> ExprPatternPair {
     )
 }
 
-pub(super) fn prefix_function(input_head: &str, output_prefix: &str, style: &RawExpressionStyle) -> ExprPatternPair { allfix_function(
+pub(super) fn variadic_prefix_function(input_head: &str, output_prefix: &str, style: &RawExpressionStyle) -> ExprPatternPair { variadic_allfix_function(
     input_head,
     &[output_prefix,style.tuple_opener()].concat(),
     style.delimiter(),
     style.tuple_closer(),
     style)
 }
+pub(super) fn prefix_function(input_head: &str, arity: usize, output_prefix: &str, style: &RawExpressionStyle) -> ExprPatternPair { allfix_function(
+    input_head,
+    arity,
+    &[output_prefix,style.tuple_opener()].concat(),
+    style.delimiter(),
+    style.tuple_closer(),
+    style)
+}
 
-pub(super) fn infix_function(input_head: &str, output_infix: &str, style: &RawExpressionStyle) -> ExprPatternPair { allfix_function(
+
+pub(super) fn variadic_infix_function(input_head: &str, output_infix: &str, style: &RawExpressionStyle) -> ExprPatternPair { variadic_allfix_function(
     input_head,
     style.tuple_opener(),
     output_infix,
     style.tuple_closer(),
     style)
 }
+pub(super) fn infix_function(input_head: &str, arity: usize, output_infix: &str, style: &RawExpressionStyle) -> ExprPatternPair { allfix_function(
+    input_head,
+    arity,
+    style.tuple_opener(),
+    output_infix,
+    style.tuple_closer(),
+    style)
+}
 
-pub(super) fn postfix_function(input_head: &str, output_postfix: &str, style: &RawExpressionStyle) -> ExprPatternPair { allfix_function(
+pub(super) fn variadic_postfix_function(input_head: &str, output_postfix: &str, style: &RawExpressionStyle) -> ExprPatternPair { variadic_allfix_function(
     input_head,
     style.tuple_opener(),
     style.delimiter(),
     &[style.tuple_closer(),output_postfix].concat(),
     style)
 }
+pub(super) fn postfix_function(input_head: &str, arity: usize, output_postfix: &str, style: &RawExpressionStyle) -> ExprPatternPair { allfix_function(
+    input_head,
+    arity,
+    style.tuple_opener(),
+    style.delimiter(),
+    &[style.tuple_closer(),output_postfix].concat(),
+    style)
+}
 
-pub(super) fn outfix_function(input_head: &str, output_left: &str, output_right: &str, style: &RawExpressionStyle) -> ExprPatternPair { allfix_function(
+pub(super) fn variadic_outfix_function(input_head: &str, output_left: &str, output_right: &str, style: &RawExpressionStyle) -> ExprPatternPair { variadic_allfix_function(
     input_head,
     output_left,
     style.delimiter(),
     output_right,
     style)
 }
+pub(super) fn outfix_n_function(input_head: &str, arity: usize, output_left: &str, output_right: &str, style: &RawExpressionStyle) -> ExprPatternPair { allfix_function(
+    input_head,
+    arity,
+    output_left,
+    style.delimiter(),
+    output_right,
+    style)
+}
 
-pub(super) fn allfix_function<'a>(input_head: &str, output_left: &str, output_infix: &str, output_right: &str, raw_style: &RawExpressionStyle) -> ExprPatternPair {
+pub(super) fn variadic_allfix_function<'a>(input_head: &str, output_left: &str, output_infix: &str, output_right: &str, raw_style: &RawExpressionStyle) -> ExprPatternPair {
     ExprPatternPair::new(
         ExprPattern::new([
             ExprPatternComponent::new_const(raw_style.tuple_opener()),
@@ -58,5 +91,29 @@ pub(super) fn allfix_function<'a>(input_head: &str, output_left: &str, output_in
             ExprPatternComponent::new_vars("A", output_infix, "B"),
             ExprPatternComponent::new_const(output_right)
         ])
+    )
+}
+pub(super) fn allfix_function<'a>(input_head: &str, arity: usize, output_left: &str, output_infix: &str, output_right: &str, raw_style: &RawExpressionStyle) -> ExprPatternPair {
+    if arity < 2 { panic!("Attempted to create n_allfix_function with n < 2") }
+    let vars_delimited = (0..arity)
+        .map(|x| [
+            ExprPatternComponent::new_var(&x.to_string()),
+            ExprPatternComponent::new_const(if x == arity { "" } else { raw_style.delimiter() })
+        ]).fold(Vec::new(), |x,y| [x.as_slice(),&y].concat());
+    let vars_infixed = (0..arity).map(|x| [
+            ExprPatternComponent::new_var(&x.to_string()),
+            ExprPatternComponent::new_const(if x == arity { "" } else { output_infix })
+        ]).fold(Vec::new(), |x,y| [x.as_slice(),&y].concat());
+    ExprPatternPair::new(
+        ExprPattern::new([
+            &[ExprPatternComponent::new_const(raw_style.tuple_opener()),ExprPatternComponent::new_const(input_head)],
+            vars_delimited.as_slice(),
+            &[ExprPatternComponent::new_const(raw_style.tuple_closer())]
+        ].concat()),
+        ExprPattern::new([
+            &[ExprPatternComponent::new_const(output_left)],
+            vars_infixed.as_slice(),
+            &[ExprPatternComponent::new_const(output_right)]
+        ].concat())
     )
 }
