5537276da432640a5f570cb266c8cd18b21622da
HolographicTripwire <josephsmagen@gmail.com>
2025-06-20 11:48:21 +0100

    Streamline function SpecialCase building implementation

--- textualization/src/structures/expressions/functional/special_case.rs
+++ textualization/src/structures/expressions/functional/special_case.rs
@@ -1,74 +1,62 @@
 use crate::structures::expressions::{patterns::{components::ExprPatternComponent, special_case::ExprPatternPair, ExprPattern}, raw::RawExpressionStyle};
 
-pub(super) fn symbol_atom<'a>(atom_id: usize, after: &str, style: &RawExpressionStyle) -> ExprPatternPair 
+pub(super) fn symbol_atom(atom_id: usize, after: &str, style: &RawExpressionStyle) -> ExprPatternPair 
     { symbol(&style.atom_style().to_id(atom_id), after) }
-pub(super) fn symbol<'a>(before: &str, after: &str) -> ExprPatternPair
-    { symbol_inner(before.to_string(), after.to_string()) }
-fn symbol_inner<'a>(before: String, after: String) -> ExprPatternPair {
+pub(super) fn symbol(before: &str, after: &str) -> ExprPatternPair {
     ExprPatternPair::new(
         ExprPattern::new([
-            ExprPatternComponent::new_const(&before)
+            ExprPatternComponent::new_const(before)
         ]), ExprPattern::new([
-            ExprPatternComponent::new_const(&after)
+            ExprPatternComponent::new_const(after)
         ])
     )
 }
 
-pub(super) fn prefix_function<'a>(function_head: &str, function_prefix: &str, style: &RawExpressionStyle) -> ExprPatternPair
-    { prefix_function_inner(function_head.to_string(), function_prefix.to_string(), style) }
-fn prefix_function_inner<'a>(function_head: String, function_prefix: String, style: &RawExpressionStyle) -> ExprPatternPair {
-    ExprPatternPair::new(
-        ExprPattern::new([
-            ExprPatternComponent::new_const(style.tuple_opener()),
-            ExprPatternComponent::new_const(&function_head),
-            ExprPatternComponent::new_const(style.delimiter()),
-            ExprPatternComponent::new_vars("A", style.delimiter(), "B"),
-            ExprPatternComponent::new_const(style.tuple_closer())
-        ]),
-        ExprPattern::new([
-            ExprPatternComponent::new_const(&function_prefix),
-            ExprPatternComponent::new_const(style.tuple_opener()),
-            ExprPatternComponent::new_vars("A", style.delimiter(), "B"),
-            ExprPatternComponent::new_const(style.tuple_closer()),
-        ])
-    )
+pub(super) fn prefix_function(input_head: &str, output_prefix: &str, style: &RawExpressionStyle) -> ExprPatternPair { allfix_function(
+    input_head,
+    &[output_prefix,style.tuple_opener()].concat(),
+    style.delimiter(),
+    style.tuple_closer(),
+    style)
 }
 
-pub(super) fn infix_function<'a>(function_head: &str, function_infix: &str, style: &RawExpressionStyle) -> ExprPatternPair
-    { infix_function_inner(function_head.to_string(), function_infix.to_string(), style) }
-fn infix_function_inner<'a>(function_head: String, function_infix: String, style: &RawExpressionStyle) -> ExprPatternPair {
-    ExprPatternPair::new(
-        ExprPattern::new([
-            ExprPatternComponent::new_const(style.tuple_opener()),
-            ExprPatternComponent::new_const(&function_head),
-            ExprPatternComponent::new_const(style.delimiter()),
-            ExprPatternComponent::new_vars("A", style.delimiter(), "B"),
-            ExprPatternComponent::new_const(style.tuple_closer())
-        ]),
-        ExprPattern::new([
-            ExprPatternComponent::new_const(style.tuple_opener()),
-            ExprPatternComponent::new_vars("A", &function_infix, "B"),
-            ExprPatternComponent::new_const(style.tuple_closer()),
-        ])
-    )
+pub(super) fn infix_function(input_head: &str, output_infix: &str, style: &RawExpressionStyle) -> ExprPatternPair { allfix_function(
+    input_head,
+    style.tuple_opener(),
+    output_infix,
+    style.tuple_closer(),
+    style)
 }
 
-pub(super) fn postfix_function<'a>(function_head: &str, function_postfix: &str, style: &RawExpressionStyle) -> ExprPatternPair
-    { postfix_function_inner(function_head.to_string(), function_postfix.to_string(), style) }
-fn postfix_function_inner<'a>(function_head: String, function_postfix: String, style: &RawExpressionStyle) -> ExprPatternPair {
+pub(super) fn postfix_function(input_head: &str, output_postfix: &str, style: &RawExpressionStyle) -> ExprPatternPair { allfix_function(
+    input_head,
+    style.tuple_opener(),
+    style.delimiter(),
+    &[style.tuple_closer(),output_postfix].concat(),
+    style)
+}
+
+pub(super) fn outfix_function(input_head: &str, output_left: &str, output_right: &str, style: &RawExpressionStyle) -> ExprPatternPair { allfix_function(
+    input_head,
+    output_left,
+    style.delimiter(),
+    output_right,
+    style)
+}
+
+pub(super) fn allfix_function<'a>(input_head: &str, output_left: &str, output_infix: &str, output_right: &str, raw_style: &RawExpressionStyle) -> ExprPatternPair {
     ExprPatternPair::new(
         ExprPattern::new([
-            ExprPatternComponent::new_const(style.tuple_opener()),
-            ExprPatternComponent::new_const(&function_head),
-            ExprPatternComponent::new_const(style.delimiter()),
-            ExprPatternComponent::new_vars("A", style.delimiter(), "B"),
-            ExprPatternComponent::new_const(style.tuple_closer())
+            ExprPatternComponent::new_const(raw_style.tuple_opener()),
+            ExprPatternComponent::new_const(input_head),
+            ExprPatternComponent::new_const(raw_style.delimiter()),
+            ExprPatternComponent::new_vars("A", raw_style.delimiter(), "B"),
+            ExprPatternComponent::new_const(raw_style.tuple_closer())
         ]),
         ExprPattern::new([
-            ExprPatternComponent::new_const(style.tuple_opener()),
-            ExprPatternComponent::new_vars("A", style.delimiter(), "B"),
-            ExprPatternComponent::new_const(style.tuple_closer()),
-            ExprPatternComponent::new_const(&function_postfix),
+            ExprPatternComponent::new_const(output_left),
+            ExprPatternComponent::new_vars("A", output_infix, "B"),
+            ExprPatternComponent::new_const(output_right)
         ])
     )
 }
