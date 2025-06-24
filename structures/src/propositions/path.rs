f69f4b98f7a441afe93f1b86e71cbf92c4a1581e
HolographicTripwire <josephsmagen@gmail.com>
2025-06-24 17:42:23 +0100

    Make SubexpressionPath a struct instead of a type

--- structures/src/propositions/path.rs
+++ structures/src/propositions/path.rs
@@ -1,10 +1,13 @@
 use crate::propositions::Expression;
 
-pub type SubexpressionPath = Box<[usize]>;
+pub struct SubexpressionPath(Box<[usize]>);
+impl <T: Into<Box<[usize]>>> From<T> for SubexpressionPath {
+    fn from(value: T) -> Self { SubexpressionPath(value.into()) }
+}
 
 impl Expression {
     pub fn get_subexpression(&self, at_postition: impl Into<SubexpressionPath>) -> Result<&Expression,()> {
-        self.get_subexpression_inner(&at_postition.into())
+        self.get_subexpression_inner(&at_postition.into().0)
     }
     
     fn get_subexpression_inner(&self, at_position: &[usize]) -> Result<&Expression,()> {
