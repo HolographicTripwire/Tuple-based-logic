4b967fa55eda91207688adc6edc58460b4f4ee6c
HolographicTripwire <josephsmagen@gmail.com>
2025-06-27 18:18:47 +0100

    Reimplement atomicity assertion with streamlined assertion syntax

--- verification/src/inference_rules/verbatim/atomicity_assertion.rs
+++ verification/src/inference_rules/verbatim/atomicity_assertion.rs
@@ -1,27 +1,23 @@
-use tbl_structures::{atoms::BuiltInAtom, propositions::Proposition};
+use tbl_structures::{atoms::BuiltInAtom, inference::Inference};
 
-use crate::{inference_rules::{TUPLE_OR_ERROR}, ProofValidationError};
+use crate::inference_rules::{assertions::{assert_expression_atomicity, assert_expression_value, assumptions_as_sized_slice, conclusions_as_sized_slice, expression_as_sized_slice}, ProofStepSpecificationError, StandardInferenceRule};
 
 use super::resolve_verbatim;
 
 /// Verify that the assumptions and the conclusion form a valid instance of atomicity assertion ("Atomic(Verbatim(a))" for any atom a)
-pub fn verify_atomicity_assertion(assumptions: &[Proposition], conclusions: &[Proposition]) -> Result<(),ProofValidationError> {
+pub fn verify_atomicity_assertion(inference: &Inference<StandardInferenceRule>) -> Result<(),ProofStepSpecificationError> {
     // Throw an error if there is not exactly one conclusion
-    let [conclusion] = conclusions else { return Err(ProofValidationError::InvalidStepSpecification) };
-    // Throw ane rror if the rule has any assumptions (this rule requires none)
-    if assumptions.len() != 0 { return Err(ProofValidationError::InvalidStepSpecification) }
+    let [conclusion] = &*conclusions_as_sized_slice(inference)?;
+    // Throw ane error if the rule has any assumptions (this rule requires none)
+    let [] = &*assumptions_as_sized_slice(inference)?;
     
     // Throw an error if there are not three expressions in the conclusion
-    let [atomicity_head, verbatim_expr] = TUPLE_OR_ERROR.as_slice(conclusion)? else { return Err(ProofValidationError::InvalidStepSpecification) };
-    
+    let [atomicity_head, verbatim_expr] = &*expression_as_sized_slice(conclusion)?;
     // Throw an error if the head of the conclusion is incorrect
-    if atomicity_head != &BuiltInAtom::Atomic.into() { return Err(ProofValidationError::InvalidStepSpecification) }
-
+    assert_expression_value(atomicity_head, &BuiltInAtom::Atomic.into())?;
     // Throw an error if the verbatim expression does not resolve to as Verbatim
     let verbatim_atom = resolve_verbatim(verbatim_expr)?;
-    
     // Throw an error if the verbatim atom is not actually an atom
-    if verbatim_atom.as_atom().is_err() { return Err(ProofValidationError::InvalidStepSpecification) };
-
+    assert_expression_atomicity(&verbatim_atom, true)?;
     Ok(())
 }
