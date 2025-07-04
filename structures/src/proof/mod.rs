a69899534de4a38d8bb55ad082803d4e6017c40a
HolographicTripwire <josephsmagen@gmail.com>
2025-07-04 14:31:36 +0100

    Implement Proof methods for CompositeProof

--- structures/src/proof/mod.rs
+++ structures/src/proof/mod.rs
@@ -12,20 +12,9 @@ pub enum Proof<Rule: InferenceRule> {
     Composite(CompositeProof<Rule>) // A composite proof made of further subproofs
 }
 
-#[derive(Clone,PartialEq,Eq,Debug)]
-pub struct CompositeProof<Rule: InferenceRule> {
-    pub assumptions: Vec<Proposition>,
-    pub subproofs: Vec<Proof<Rule>>,
-    pub explicit_conclusions: Vec<Proposition>,
-}
-impl <Rule: InferenceRule> CompositeProof<Rule> {
-    pub fn new(assumptions: Vec<Proposition>, subproofs: Vec<Proof<Rule>>, explicit_conclusions: Vec<Proposition>) -> Self
-        { Self { assumptions, subproofs, explicit_conclusions } }
-}
-
 impl <Rule: InferenceRule> Proof<Rule> {
     /// Get the premises of this [Proof]
-    pub fn premises(&self) -> &Vec<Proposition> { match self {
+    pub fn assumptions(&self) -> &Vec<Proposition> { match self {
         Proof::Atomic(proof_step) => &proof_step.assumptions,
         Proof::Composite(composite) => &composite.assumptions,
     }}
@@ -44,12 +33,32 @@ impl <Rule: InferenceRule> Proof<Rule> {
 
     fn conclusions(&self) -> HashSet<&Proposition> { match self {
         Proof::Atomic(inference) => HashSet::from_iter(inference.conclusions.iter()),
-        Proof::Composite(composite) => {
-            composite.subproofs.iter().fold(HashSet::from_iter(&composite.explicit_conclusions), 
+        Proof::Composite(composite) => { composite.conclusions() },
+    }}
+}
+
+#[derive(Clone,PartialEq,Eq,Debug)]
+pub struct CompositeProof<Rule: InferenceRule> {
+    pub assumptions: Vec<Proposition>,
+    pub subproofs: Vec<Proof<Rule>>,
+    pub explicit_conclusions: Vec<Proposition>,
+}
+impl <Rule: InferenceRule> CompositeProof<Rule> {
+    pub fn new(assumptions: Vec<Proposition>, subproofs: Vec<Proof<Rule>>, explicit_conclusions: Vec<Proposition>) -> Self
+        { Self { assumptions, subproofs, explicit_conclusions } }
+
+        pub fn assumptions(&self) -> &Vec<Proposition> { &self.assumptions }
+        pub fn subproofs(&self) -> &Vec<Proof<Rule>> { &self.subproofs }
+        pub fn explicit_conclusions(&self) -> &Vec<Proposition> { &self.explicit_conclusions }
+        pub fn implicit_conclusions(&self) -> HashSet<&Proposition> {
+            let explicits = HashSet::from_iter(self.explicit_conclusions());
+            self.conclusions().difference(&explicits).cloned().collect()
+        }
+        pub fn conclusions(&self) -> HashSet<&Proposition> { 
+            self.subproofs.iter().fold(HashSet::from_iter(&self.explicit_conclusions), 
             |mut acc,next| { acc.extend(next.conclusions().iter()); acc }
             )
-        },
-    }}
+        }
 }
 
 #[cfg(test)]
