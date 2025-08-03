f97cf2fa6109f77d699fa9bceb6a27822521cb94
HolographicTripwire <josephsmagen@gmail.com>
2025-08-03 17:43:44 +0100

    Reimplement SubproofPath stringification using DisplayExt to allow for continued use of type alias

--- structures/src/lib.rs
+++ structures/src/lib.rs
@@ -2,3 +2,8 @@ pub mod atoms; // The atoms which make up expressions
 pub mod expressions; // Propositions; which represent an expression with truth values
 pub mod inference; // Single steps of a larger proof
 pub mod proof; // Proofs which show that some Propositions can be reached from others
+
+/// A copy of the [Display](fmt::Display) trait which can be implemented on external types from within this crate
+pub trait DisplayExt {
+    fn display(&self) -> String;
+}
--- structures/src/proof/subproof_path.rs
+++ structures/src/proof/subproof_path.rs
@@ -1,8 +1,8 @@
 use std::fmt::Display;
 
-use path_lib::{obj_at_path::{ObjAtPath, OwnedObjAtPath}, paths::{PathPrimitive, PathSeries}, HasChildren};
+use path_lib::{obj_at_path::{ObjAtPath, OwnedObjAtPath}, paths::{PathPrimitive, PathSeries}, HasChildren, Path};
 
-use crate::{inference::InferenceRule, proof::{CompositeProof, Proof}};
+use crate::{inference::InferenceRule, proof::{CompositeProof, Proof}, DisplayExt};
 
 #[derive(Clone,PartialEq,Eq,Debug)]
 /// Identifies a particular step iwthin a [`Proof`], and can be given to such a [`Proof`] to retreive the [`SubProof`] at that step
@@ -13,19 +13,12 @@ impl Display for AtomicSubproofPath {
         { write!(f, "{}", self.0) }
 }
 
-#[derive(Clone,PartialEq,Eq,Debug)]
-pub struct SubproofPath(pub PathSeries<AtomicSubproofPath>);
-impl SubproofPath {
-    pub fn empty() -> Self { Self(PathSeries::empty()) }
-    pub fn new<I: Into<AtomicSubproofPath>>(series: impl IntoIterator<Item=I>) -> Self { Self(PathSeries::new(series)) }
-}
-impl Display for SubproofPath {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0.paths().iter()
+pub type SubproofPath = PathSeries<AtomicSubproofPath>;
+impl DisplayExt for SubproofPath {
+    fn display(&self) -> String {
+        self.paths().iter()
             .map(|atom| atom.to_string())
-            .collect::<Vec<String>>()
-            .join(".")
-        )
+            .collect::<Vec<String>>().join(".")
     }
 }
 
