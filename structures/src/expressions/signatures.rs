09e85f9e1d72620a1b3ec0b407660ac1bb3b6f59
HolographicTripwire <josephsmagen@gmail.com>
2025-10-09 22:29:40 +0100

    Improve efficiency of ExprSignatures construction

--- structures/src/expressions/signatures.rs
+++ structures/src/expressions/signatures.rs
@@ -14,31 +14,45 @@ impl ExprSignatures {
 
 impl Into<Expression> for ExprSignatures {
     fn into(self) -> Expression
-        { self.structure.into_expression_inner(&self.contents, &mut 0) }
+        { Self::into_expression_inner(self.structure, &self.contents, &mut 0) }
+}
+
+impl ExprSignatures {
+    fn from_expression_inner(expr: Expression, contents: &mut ExprContentsSignature) -> ExprStructureSignature {
+        match expr {
+            Expression::Atomic(atom_id) => {
+                contents.0.push(atom_id);
+                ExprStructureSignature::Atom
+            }, Expression::Tuple(expressions) => {
+                ExprStructureSignature::Tuple(expressions
+                    .into_iter()
+                    .map(|expr| Self::from_expression_inner(expr, contents))
+                    .collect()
+                )
+            },
+        }
+    }
+
+    fn into_expression_inner(structure: ExprStructureSignature, contents: &ExprContentsSignature, atom_ix: &mut usize) -> Expression {
+        match structure {
+            ExprStructureSignature::Atom => { 
+                let e = Expression::Atomic(contents.0[*atom_ix]);
+                *atom_ix += 1;
+                e
+            }, ExprStructureSignature::Tuple(signatures) => Expression::Tuple(signatures
+                .into_iter()
+                .map(|structure| Self::into_expression_inner(structure, contents, atom_ix))
+                .collect()
+            )
+        }
+    }
 }
 
 impl From<Expression> for ExprSignatures {
     fn from(expr: Expression) -> Self {
-        match expr {
-            Expression::Atomic(atom_id) => ExprSignatures {
-                structure: ExprStructureSignature::Atom,
-                contents: ExprContentsSignature(vec![atom_id])
-            }, Expression::Tuple(expressions) => {
-                let (structures, contents): (Vec<ExprStructureSignature>, Vec<ExprContentsSignature>) = expressions
-                    .into_iter()
-                    .map(|e| { let s: ExprSignatures = e.into(); (s.structure, s.contents)})
-                    .unzip();
-                ExprSignatures {
-                    structure: ExprStructureSignature::Tuple(structures),
-                    contents: ExprContentsSignature(contents
-                        .into_iter()
-                        .map(|x| x.0)
-                        .flatten()
-                        .collect()
-                    ),
-                }
-            }
-        }
+        let mut contents = ExprContentsSignature(vec![]);
+        let structure = Self::from_expression_inner(expr, &mut contents);
+        ExprSignatures { structure, contents }
     }
 }
 
@@ -47,22 +61,6 @@ impl From<Expression> for ExprSignatures {
 pub enum ExprStructureSignature {
     Atom,
     Tuple(Vec<ExprStructureSignature>)
-}
-
-impl ExprStructureSignature {
-    fn into_expression_inner(self, contents: &ExprContentsSignature, atom_ix: &mut usize) -> Expression {
-        match self {
-            ExprStructureSignature::Atom => { 
-                let e = Expression::Atomic(contents.0[*atom_ix]);
-                *atom_ix += 1;
-                e
-            }, ExprStructureSignature::Tuple(signatures) => Expression::Tuple(signatures
-                .into_iter()
-                .map(|signature| signature.into_expression_inner(contents, atom_ix))
-                .collect()
-            )
-        }
-    }
 }
 
 #[derive(Clone,PartialEq,Eq,Hash,Debug)]
