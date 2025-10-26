07b0077625819ecbf33e5c624055db1bb38d8f6c
HolographicTripwire <josephsmagen@gmail.com>
2025-10-26 18:15:58 +0000

    Implement From over a variety of path types for ExpressionInProof and PropositionInProof

--- structures/src/path_composites/expression_in_proof.rs
+++ structures/src/path_composites/expression_in_proof.rs
@@ -1,8 +1,8 @@
 use std::fmt::Display;
 
-use path_lib::{obj_at_path::{ObjAtPath, OwnedObjAtPath}, Path};
+use path_lib::{obj_at_path::{ObjAtPath, OwnedObjAtPath}, paths::PathPair, Path};
 
-use crate::{expressions::{Expression, ExpressionInExpressionPath}, proof::{InferenceInProofPath, PropositionInInferencePath}, DisplayExt};
+use crate::{expressions::{AtomicExpressionInExpressionPath, Expression, ExpressionInExpressionPath}, proof::{InferenceInProofPath, PropositionInInferencePath}, DisplayExt};
 
 #[derive(Clone,PartialEq,Eq)]
 pub struct ExpressionInProofPath{
@@ -20,3 +20,52 @@ impl Display for ExpressionInProofPath {
 
 pub type ExpressionInProof<'a> = ObjAtPath<'a,Expression,ExpressionInProofPath>;
 pub type OwnedExpressionInProof = OwnedObjAtPath<Expression,ExpressionInProofPath>;
+
+mod from {
+    use path_lib::paths::PathSeries;
+
+    use crate::path_composites::PropositionInProofPath;
+
+    use super::*;
+
+    impl From<PropositionInProofPath> for ExpressionInProofPath {
+        fn from(path: PropositionInProofPath) -> Self { Self {
+            step_path: path.step_path,
+            proposition_path: path.proposition_path,
+            subexpression_path: ExpressionInExpressionPath::empty(),
+        }}
+    }
+    impl From<PathPair<ExpressionInProofPath,AtomicExpressionInExpressionPath>> for ExpressionInProofPath {
+        fn from(mut value: PathPair<ExpressionInProofPath,AtomicExpressionInExpressionPath>) -> Self { 
+            value.left.subexpression_path.append(value.right);
+            value.left
+        }
+    }
+    impl From<PathPair<ExpressionInProofPath,ExpressionInExpressionPath>> for ExpressionInProofPath {
+        fn from(mut value: PathPair<ExpressionInProofPath,ExpressionInExpressionPath>) -> Self { 
+            value.left.subexpression_path.append_all(value.right.into_paths());
+            value.left
+        }
+    }
+    impl From<PathPair<PropositionInProofPath,ExpressionInExpressionPath>> for ExpressionInProofPath {
+        fn from(value: PathPair<PropositionInProofPath,ExpressionInExpressionPath>) -> Self { Self {
+                step_path: value.left.step_path,
+                proposition_path: value.left.proposition_path,
+                subexpression_path: value.right
+        }}
+    }
+    impl From<PathPair<PropositionInProofPath,AtomicExpressionInExpressionPath>> for ExpressionInProofPath {
+        fn from(value: PathPair<PropositionInProofPath,AtomicExpressionInExpressionPath>) -> Self { Self {
+                step_path: value.left.step_path,
+                proposition_path: value.left.proposition_path,
+                subexpression_path: PathSeries::new([value.right])
+        }}
+    }
+    impl From<PathPair<InferenceInProofPath,PropositionInInferencePath>> for ExpressionInProofPath {
+        fn from(value: PathPair<InferenceInProofPath,PropositionInInferencePath>) -> Self { Self {
+            step_path: value.left,
+            proposition_path: value.right,
+            subexpression_path: ExpressionInExpressionPath::empty(),
+        }}
+    }
+}
--- structures/src/path_composites/proposition_in_proof.rs
+++ structures/src/path_composites/proposition_in_proof.rs
@@ -1,23 +1,35 @@
 use std::fmt::Display;
 
-use path_lib::{obj_at_path::{ObjAtPath, OwnedObjAtPath}, Path};
+use path_lib::{obj_at_path::{ObjAtPath, OwnedObjAtPath}, Path, paths::PathPair};
 
-use crate::{expressions::Proposition, proof::{ProofInProofPath, PropositionInInferencePath}, DisplayExt};
+use crate::{expressions::Proposition, proof::{InferenceInProofPath, PropositionInInferencePath}};
 
 #[derive(Clone,PartialEq,Eq)]
 pub struct PropositionInProofPath {
-    pub step_path: ProofInProofPath,
+    pub step_path: InferenceInProofPath,
     pub proposition_path: PropositionInInferencePath,
 }
 impl PropositionInProofPath {
-    pub fn new(step: ProofInProofPath, proposition: PropositionInInferencePath) -> Self { Self { step_path: step, proposition_path: proposition } }
+    pub fn new(step: InferenceInProofPath, proposition: PropositionInInferencePath) -> Self { Self { step_path: step, proposition_path: proposition } }
 }
 impl Path for PropositionInProofPath {}
 impl Display for PropositionInProofPath {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f,"{}{}",self.step_path.display(),self.proposition_path)
+        write!(f,"{}{}",self.step_path,self.proposition_path)
     }
 }
 
-pub type PropositionInProof<'a> = ObjAtPath<'a,Proposition,PropositionInInferencePath>;
-pub type OwnedPropositionInProof = OwnedObjAtPath<Proposition,PropositionInInferencePath>;
+pub type PropositionInProof<'a> = ObjAtPath<'a,Proposition,PropositionInProofPath>;
+pub type OwnedPropositionInProof = OwnedObjAtPath<Proposition,PropositionInProofPath>;
+
+mod from {
+    use crate::proof::InferenceInProofPath;
+
+    use super::*;
+
+    impl From<PathPair<InferenceInProofPath,PropositionInInferencePath>> for PropositionInProofPath {
+        fn from(pair: PathPair<InferenceInProofPath,PropositionInInferencePath>) -> Self { 
+            Self::new(pair.left, pair.right)
+        }
+    }
+}
