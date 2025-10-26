4b1920cf2b571c29df894323c4f16bfe7d190c54
HolographicTripwire <josephsmagen@gmail.com>
2025-10-26 18:43:05 +0000

    Implement assertions for proof_step

--- verification/src/assertions/proof_step/explicit_conclusion_count.rs
+++ verification/src/assertions/proof_step/explicit_conclusion_count.rs
@@ -1,29 +1,28 @@
-use path_lib::obj_at_path::OwnedObjAtPath;
-use tbl_structures::{inference::InferenceRule, proof::{OwnedSubproofInProof, Proof, ProofStep, SubproofPath}, DisplayExt};
+use tbl_structures::{inference::InferenceRule, proof::{OwnedInferenceInProof, ProofStep}};
 
 use crate::errors::specification_error::{NaryPredicate, NaryStringifier, ProofStepSpecificationError, StringifiablePredicate};
 
 
 /// Get a [Predicate](NaryPredicate) which takes a [Subproof](OwnedSubproofInProof) and checks if it has as expected_count assumptions
-fn explicit_conclusion_count_predicate<'a,Rule:InferenceRule>(expected_count: usize) -> impl NaryPredicate<'a,1,OwnedSubproofInProof<Rule>> {
-    move |o: [OwnedObjAtPath<Proof<Rule>, SubproofPath>; 1]| 
+fn explicit_conclusion_count_predicate<'a,Rule:InferenceRule>(expected_count: usize) -> impl NaryPredicate<'a,1,OwnedInferenceInProof<Rule>> {
+    move |o: [OwnedInferenceInProof<Rule>; 1]| 
     o[0].obj().explicit_conclusion_paths().into_iter().count() == expected_count
 }
 /// Get a [Stringifier](NaryPredicate) which takes a [Subproof](OwnedSubproofInProof) and returns an error message saying that this subproof does not have expected_count explicit conclusions
-fn explicit_conclusion_count_stringifier<'a,Rule:InferenceRule>(expected_count: usize) -> impl NaryStringifier<'a,1,OwnedSubproofInProof<Rule>> {
-    move |o: [OwnedObjAtPath<Proof<Rule>, SubproofPath>; 1]| format!(
+pub fn explicit_conclusion_count_stringifier<'a,Rule:InferenceRule>(expected_count: usize) -> impl NaryStringifier<'a,1,OwnedInferenceInProof<Rule>> {
+    move |o: [OwnedInferenceInProof<Rule>; 1]| format!(
         "Proof at step {step} has wrong number of explicit conclusions (expected {num_expected}; found {num_actual}",
-        step=o[0].path().display(), num_expected=expected_count,
+        step=o[0].path().to_string(), num_expected=expected_count,
         num_actual=o[0].obj().explicit_conclusion_paths().into_iter().count()
     )
 }
 /// Get a [Checker](StringifiablePredicate) which takes a [Subproof](OwnedSubproofInProof) and returns an error message if this subproof does not have expected_count explicit conclusions
-pub fn explicit_conclusion_count_check<'a,Rule:InferenceRule>(expected_count: usize) -> StringifiablePredicate<'a,1,OwnedSubproofInProof<Rule>> { StringifiablePredicate::new(
+pub fn explicit_conclusion_count_check<'a,Rule:InferenceRule>(expected_count: usize) -> StringifiablePredicate<'a,1,OwnedInferenceInProof<Rule>> { StringifiablePredicate::new(
     explicit_conclusion_count_predicate(expected_count),
     explicit_conclusion_count_stringifier(expected_count),
 )}
 /// Check that the provided [Subproof](OwnedSubproofInProof) has expected_count explicit conclusions, returning an error otherwise
-pub fn assert_explicit_conclusion_count<'a,Rule:InferenceRule>(step: OwnedSubproofInProof<Rule>, expected_count: usize) -> Result<(), ProofStepSpecificationError<'a>> {
+pub fn assert_explicit_conclusion_count<'a,Rule:InferenceRule>(step: OwnedInferenceInProof<Rule>, expected_count: usize) -> Result<(), ProofStepSpecificationError<'a>> {
     explicit_conclusion_count_check(expected_count).evaluate([step])
         .map_err(|assertion| ProofStepSpecificationError::from_inner(assertion))
 }
--- verification/src/assertions/proof_step/mod.rs
+++ verification/src/assertions/proof_step/mod.rs
@@ -1,5 +1,39 @@
 mod assumption_count;
 mod explicit_conclusion_count;
 
-pub use assumption_count::assert_assumption_count;
-pub use explicit_conclusion_count::assert_explicit_conclusion_count;
+pub use assumption_count::*;
+pub use explicit_conclusion_count::*;
+use path_lib::obj_at_path::ObjAtPathWithChildren;
+use tbl_structures::{inference::InferenceRule, path_composites::OwnedPropositionInProof, proof::OwnedInferenceInProof};
+
+use crate::errors::{specification_error::NaryStringifier, ProofStepSpecificationError};
+
+pub fn assumptions_as_slice<Rule: InferenceRule>(inference: &OwnedInferenceInProof<Rule>) -> Vec<OwnedPropositionInProof> {
+    inference.get_located_children_owned()
+        .into_iter()
+        .map(|obj| obj.replace_path(|p| p.into()))
+        .collect::<Vec<OwnedPropositionInProof>>()
+}
+
+pub fn assumptions_as_sized_slice<'a,const expected_size: usize,Rule: InferenceRule>(inference: &OwnedInferenceInProof<Rule>) -> Result<Box<[OwnedPropositionInProof; expected_size]>,ProofStepSpecificationError<'a>> {
+    match assumptions_as_slice(inference)
+        .try_into() {
+            Ok(a) => Ok(a),
+            Err(_) => Err(ProofStepSpecificationError::from_inner(assumption_count_stringifier(expected_size).assign([inference.to_owned()]))),
+        }
+}
+
+pub fn explicit_conclusions_as_slice<Rule: InferenceRule>(inference: &OwnedInferenceInProof<Rule>) -> Vec<OwnedPropositionInProof> {
+    inference.get_located_children_owned()
+        .into_iter()
+        .map(|obj| obj.replace_path(|p| p.into()))
+        .collect::<Vec<OwnedPropositionInProof>>()
+}
+
+pub fn explicit_conclusions_as_sized_slice<'a,const expected_size: usize,Rule: InferenceRule>(inference: &OwnedInferenceInProof<Rule>) -> Result<Box<[OwnedPropositionInProof; expected_size]>,ProofStepSpecificationError<'a>> {
+    match explicit_conclusions_as_slice(inference)
+        .try_into() {
+            Ok(a) => Ok(a),
+            Err(_) => Err(ProofStepSpecificationError::from_inner(explicit_conclusion_count_stringifier(expected_size).assign([inference.to_owned()]))),
+        }
+}
