556d82040618a327daafb7718aa9aee2265cd7d7
HolographicTripwire <josephsmagen@gmail.com>
2025-10-28 20:03:47 +0000

    Fix comment references in verification/assertions/proof_step

--- verification/src/assertions/proof_step/explicit_conclusion_count.rs
+++ verification/src/assertions/proof_step/explicit_conclusion_count.rs
@@ -3,12 +3,12 @@ use tbl_structures::{inference::InferenceRule, proof::{OwnedInferenceInProof, Pr
 use crate::errors::specification_error::{NaryPredicate, NaryStringifier, ProofStepSpecificationError, StringifiablePredicate};
 
 
-/// Get a [Predicate](NaryPredicate) which takes a [Subproof](OwnedSubproofInProof) and checks if it has as expected_count assumptions
+/// Get a [Predicate](NaryPredicate) which takes an [Inference](OwnedInferenceInProof) and checks if it has as expected_count assumptions
 fn explicit_conclusion_count_predicate<'a,Rule:InferenceRule>(expected_count: usize) -> impl NaryPredicate<'a,1,OwnedInferenceInProof<Rule>> {
     move |o: [OwnedInferenceInProof<Rule>; 1]| 
     o[0].obj().explicit_conclusion_paths().into_iter().count() == expected_count
 }
-/// Get a [Stringifier](NaryPredicate) which takes a [Subproof](OwnedSubproofInProof) and returns an error message saying that this subproof does not have expected_count explicit conclusions
+/// Get a [Stringifier](NaryPredicate) which takes an [Inference](OwnedInferenceInProof) and returns an error message saying that this subproof does not have expected_count explicit conclusions
 pub fn explicit_conclusion_count_stringifier<'a,Rule:InferenceRule>(expected_count: usize) -> impl NaryStringifier<'a,1,OwnedInferenceInProof<Rule>> {
     move |o: [OwnedInferenceInProof<Rule>; 1]| format!(
         "Proof at step {step} has wrong number of explicit conclusions (expected {num_expected}; found {num_actual}",
@@ -16,12 +16,12 @@ pub fn explicit_conclusion_count_stringifier<'a,Rule:InferenceRule>(expected_cou
         num_actual=o[0].obj().explicit_conclusion_paths().into_iter().count()
     )
 }
-/// Get a [Checker](StringifiablePredicate) which takes a [Subproof](OwnedSubproofInProof) and returns an error message if this subproof does not have expected_count explicit conclusions
+/// Get a [Checker](StringifiablePredicate) which takes a [Inference](OwnedInferenceInProof) and returns an error message if this subproof does not have expected_count explicit conclusions
 pub fn explicit_conclusion_count_check<'a,Rule:InferenceRule>(expected_count: usize) -> StringifiablePredicate<'a,1,OwnedInferenceInProof<Rule>> { StringifiablePredicate::new(
     explicit_conclusion_count_predicate(expected_count),
     explicit_conclusion_count_stringifier(expected_count),
 )}
-/// Check that the provided [Subproof](OwnedSubproofInProof) has expected_count explicit conclusions, returning an error otherwise
+/// Check that the provided [Inference](OwnedInferenceInProof) has expected_count explicit conclusions, returning an error otherwise
 pub fn assert_explicit_conclusion_count<'a,Rule:InferenceRule>(step: OwnedInferenceInProof<Rule>, expected_count: usize) -> Result<(), ProofStepSpecificationError<'a>> {
     explicit_conclusion_count_check(expected_count).evaluate([step])
         .map_err(|assertion| ProofStepSpecificationError::from_inner(assertion))
