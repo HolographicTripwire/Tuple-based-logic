97c1db148b6a5fc5dd49dcde7dee363db81b81a5
HolographicTripwire <josephsmagen@gmail.com>
2025-11-05 08:11:37 +0000

    Implement TryFrom<ProofInProof> for InferenceInProof (and owned equivalent)

--- structures/src/proof/inference_path.rs
+++ structures/src/proof/inference_path.rs
@@ -2,7 +2,7 @@ use std::fmt::Display;
 
 use path_lib::{obj_at_path::{ObjAtPath, OwnedObjAtPath}, Path};
 
-use crate::{DisplayExt, inference::{Inference, InferenceRule}, proof::{ProofInProofPath}};
+use crate::{DisplayExt, inference::{Inference, InferenceRule}, proof::{OwnedProofInProof, Proof, ProofInProof, ProofInProofPath}};
 
 #[derive(Clone,PartialEq,Eq)]
 pub struct InferenceInProofPath(pub ProofInProofPath);
@@ -17,3 +17,25 @@ impl Display for InferenceInProofPath {
 pub struct InferenceInProof<'a,Rule: InferenceRule>(pub ObjAtPath<'a,Inference<Rule>,InferenceInProofPath>);
 #[derive(Clone,PartialEq,Eq)]
 pub struct OwnedInferenceInProof<Rule: InferenceRule>(pub OwnedObjAtPath<Inference<Rule>,InferenceInProofPath>);
+
+impl <'a,Rule: InferenceRule> TryFrom<ProofInProof<'a,Rule>> for InferenceInProof<'a,Rule> {
+    type Error = ProofInProof<'a,Rule>;
+
+    fn try_from(value: ProofInProof<'a,Rule>) -> Result<Self, Self::Error> {
+        let (obj,path) = value.0.into_obj_and_path();
+        if let Proof::Atomic(inference) = obj
+            { Ok(InferenceInProof(ObjAtPath::from_at(inference, InferenceInProofPath(path)))) }
+        else { Err(ProofInProof(ObjAtPath::from_at(obj, path))) }
+    }
+}
+
+impl <Rule: InferenceRule> TryFrom<OwnedProofInProof<Rule>> for OwnedInferenceInProof<Rule> {
+    type Error = OwnedProofInProof<Rule>;
+
+    fn try_from(value: OwnedProofInProof<Rule>) -> Result<Self, Self::Error> {
+        let (obj,path) = value.0.into_obj_and_path();
+        if let Proof::Atomic(inference) = obj
+            { Ok(OwnedInferenceInProof(OwnedObjAtPath::from_at(inference, InferenceInProofPath(path)))) }
+        else { Err(OwnedProofInProof(OwnedObjAtPath::from_at(obj, path))) }
+    }
+}
